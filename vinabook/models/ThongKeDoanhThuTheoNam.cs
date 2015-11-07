ac8dbf0499b5d4c83e582e054855dacce888b5cf
nguyentrucxjnh@gmail.com <nguyentrucxjnh@gmail.com>
2015-11-07 19:31:44 +0700

    thanh cờ hó nó đụng zô giờ bị lỗi rồi nè, cờ hó thanh

---
+++ vinabook/models/ThongKeDoanhThuTheoNam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vinabook.Models
+{
+    public class ThongKeDoanhThuTheoNam
+    {
+        public string Nam { get; set; }
+        public string TongTien { get; set; }
+    }
+}
