47dbd4398896119e137e571a9ca786c8a32b30e9
tranthanh <quocthanh2694@gmail.com>
2015-11-07 15:59:39 +0700

    Thong ke doanh thu(Nam-Thang)

---
+++ vinabook/models/ThongKeDoanhThuTheoThang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinabook.Models
+{
+    public class ThongKeDoanhThuTheoThang
+    {
+        public string Thang { get; set; }
+        public string TongTien { get; set; }
+    }
+}
