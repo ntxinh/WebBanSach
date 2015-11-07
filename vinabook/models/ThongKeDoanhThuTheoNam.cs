75ec2b71071268bfc2948f499cea321b0f3707d9
tranthanh <quocthanh2694@gmail.com>
2015-11-07 20:46:28 +0700

    a

---
+++ vinabook/models/ThongKeDoanhThuTheoNam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinabook.Models
+{
+    public class ThongKeDoanhThuTheoNam
+    {
+        public string Nam { get; set; }
+        public string TongTien { get; set; }
+    }
+}
